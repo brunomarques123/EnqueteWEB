6ab7f5ba57c4314863d5df11ca5aa5e64da5b15e
Bruno Marques <b_marques85@live.com>
2022-10-14 23:05:38 -0300

    Adicionar arquivos de projeto.

---
+++ EnqueteWEB/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EnqueteWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnqueteWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Responder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Responder(Resposta resposta)
+        {
+            Repositorio.AdicionarResposta(resposta);
+            return View("Obrigado");
+        }
+
+        public IActionResult Resultado()
+        {
+            return View(Repositorio.Respostas);
+        }
+
+    }
+}
---
+++ EnqueteWEB/Models/Repositorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnqueteWEB.Models
+{
+    public class Repositorio
+    {
+        private static List<Resposta> respostas = new List<Resposta>();
+        public static IEnumerable<Resposta>Respostas { get { return respostas; } }
+        public static void AdicionarResposta(Resposta resposta)
+        {
+            respostas.Add(resposta);
+        }
+        
+        static Repositorio()
+        {
+            respostas.Add(new Resposta()
+            { Nome = "Fulano", Email = "fulano@gmail.com", Sim = true });
+            respostas.Add(new Resposta()
+            { Nome = "Ciclano", Email = "Ciclano@gmail.com", Sim = false });
+            respostas.Add(new Resposta()
+            { Nome = "Beltrano", Email = "Beltrano@gmail.com", Sim = true });
+        }
+    }
+       
+}
+
